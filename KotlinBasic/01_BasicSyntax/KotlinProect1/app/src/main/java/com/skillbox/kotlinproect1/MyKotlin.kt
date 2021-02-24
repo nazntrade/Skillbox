3941c1f25bab221d8c79fe3f565f7ab750bb0c28
Andrew Nazentsev <nazntrade@gmail.com>
2021-02-24 18:01:42 +0300

    2.5 Homework 1

---
+++ KotlinBasic/01_BasicSyntax/KotlinProect1/app/src/main/java/com/skillbox/kotlinproect1/MyKotlin.kt
@@ -0,0 +1,23 @@
+
+fun main() {
+    val firstName: String
+    firstName = "Andrew"
+    val lastNameString = "Nazentsev"
+
+    var heightInt = 175
+    var weight = 100.54f
+
+    val isChild1 = "is a Child"
+    val isChild2 = "is not a Child"
+    val isChild: Boolean = (heightInt < 140 || weight < 40)
+    val isChild4 = if (isChild == true) isChild1 else isChild2
+
+    val info = """
+        Firstname: ${firstName} 
+        Lastname: ${lastNameString} 
+        Height: ${heightInt} 
+        Weight: ${weight} 
+        ${isChild4}
+        """
+    print(info)
+}
