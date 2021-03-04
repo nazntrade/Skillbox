1ff64ed2a80c95b7484277599cd626011767a0a8
Andrew Nazentsev <nazntrade@gmail.com>
2021-03-04 10:34:14 +0300

    homeWork2.8

--- KotlinBasic/02_Functions/HomeWork2.8/app/src/main/java/com/skillbox/homework28/homeWork2.8.kt
+++ KotlinBasic/02_Functions/HomeWork2.8/app/src/main/java/com/skillbox/homework28/homeWork2.8.kt
@@ -3,21 +3,23 @@ package com.skillbox.homework28
 import kotlin.math.sqrt
 
 /*the equation - a * x^2 + b * x + c = 0*/
-fun main () {
-        val solutionSum = solveEquation(a=-8, b=14, c=73)
+fun main() {
+    val solutionSum = solveEquation(a = -8, b = 14, c = 73)
     println("root sum is $solutionSum")
 }
+
 fun solveEquation(a: Int, b: Int, c: Int): Double {
 //*finding discriminant
     val d = (b * b - 4.0 * a * c)
-        if (d < 0) {
+    if (d < 0) {
         println("equation has no roots")
+        return Double.NaN
     }
 //finding roots
 
-        val x1 = (sqrt(d) - b) / 2.0 * a
-        val x2 = (-sqrt(d) - b) / 2.0 * a
-        println("""
+    val x1 = (sqrt(d) - b) / 2.0 * a
+    val x2 = (-sqrt(d) - b) / 2.0 * a
+    println("""
             x1=${x1}            
             x2=${x2}""")
     return (x1 + x2)
