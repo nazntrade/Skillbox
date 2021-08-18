b870ba5fef7a62ab5361483afa340dd5a87de9a2
Nazentcev Andrey <nazntrade@gmail.com>
2021-08-18 07:58:58 +0300

    HW9_18.08.2021

--- Android_Projects_All_Naz/AndroidStudio/Lessons10/app/src/main/java/com/skillbox/lessons10/DynamicActivity.kt
+++ Android_Projects_All_Naz/AndroidStudio/Lessons10/app/src/main/java/com/skillbox/lessons10/DynamicActivity.kt
@@ -2,6 +2,7 @@ package com.skillbox.lessons10
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
@@ -19,27 +20,20 @@ class DynamicActivity : AppCompatActivity() {
         val addButton = findViewById<Button>(R.id.addButton)
         val textInput = findViewById<TextView>(R.id.textInput)
         val container = findViewById<LinearLayout>(R.id.container)
+        val textView = findViewById<TextView>(R.id.textView)
+        val deleteButton = findViewById<Button>(R.id.deleteButton)
         //
         // а потом уже все это использую
 
         addButton.setOnClickListener {
             val textToAdd = textInput.text.toString()
-            val textViewToAdd = TextView(this).apply {
-                text = textToAdd
-                layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                ).apply {
-                    gravity = when (Random.nextInt(3)) {
-                        0 -> Gravity.CENTER
-                        1 -> Gravity.END
-                        else -> Gravity.START
-                    }
+            val view = layoutInflater.inflate(R.layout.item_text, container, false)
+            view.apply {
+                textView.text = textToAdd
+                deleteButton.setOnClickListener {
+                    container.removeView()
                 }
             }
-
-            container.addView(textViewToAdd)
-
         }
     }
 }
