87c9422fed0ffe853ea442b175e7b8d513e6970e
Andrew Nazentsev <nazntrade@gmail.com>
2021-08-24 12:11:45 +0300

    hw10_v.3.0

--- ViewAndLayout/app/src/main/java/com/skillbox/homework10/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/skillbox/homework10/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 checkButton()
             }
+
             override fun afterTextChanged(p0: Editable?) {}
         })
 
@@ -33,6 +34,7 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 checkButton()
             }
+
             override fun afterTextChanged(p0: Editable?) {}
         })
 
