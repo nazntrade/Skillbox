c6bf04b0e7f32a7d1facb15efcbd98e8a2906e9a
Nazentcev Andrey <nazntrade@gmail.com>
2021-08-31 08:58:26 +0300

    lesson_11.5_30.08.2021

--- Android_Projects_All_Naz/AndroidStudio/Lesson11.5/app/src/main/java/com/skillbox/lesson11_5/MainActivity.kt
+++ Android_Projects_All_Naz/AndroidStudio/Lesson11.5/app/src/main/java/com/skillbox/lesson11_5/MainActivity.kt
@@ -1,23 +1,91 @@
 package com.skillbox.lesson11_5
 
 import android.os.Bundle
+import android.telephony.mbms.StreamingServiceInfo
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.skillbox.lesson11_5.databinding.ActivityMainBinding
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val users = listOf(
+        "User1",
+        "User2",
+        "Unknown"
+    )
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initToolbar()
+    }
+
+
+    private fun toast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initToolbar() {
+
         binding.toolbar.setNavigationOnClickListener {
-            Toast.makeText(this, "Navigation clicked", Toast.LENGTH_SHORT).show()
+            toast("Navigation clicked")
         }
 
+        binding.toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_1 -> {
+                    toast("action_1 clicked")
+                    true
+                }
+                R.id.action_2 -> {
+                    toast("action_2 clicked")
+                    true
+                }
+                R.id.action_3 -> {
+                    toast("action_3 clicked")
+                    true
+                }
+                else -> false
+            }
+        }
 
+        val searchItem = binding.toolbar.menu.findItem(R.id.action_search)
+        searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                binding.expendTextView.text = "search expanded"
+                return true
+            }
 
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                binding.expendTextView.text = "search collapsed"
+                return true
+
+            }
+
+        })
+
+        (searchItem.actionView as android.widget.SearchView).setOnQueryTextListener(
+            object : android.widget.SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    users.filter { it.contains(newText ?: "", ignoreCase = true) }
+                        .joinToString()
+                        .let {
+                            binding.searchResultTextView.text = it
+                        }
+                    return true
+                }
+
+            })
 
     }
 }
+
