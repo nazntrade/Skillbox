8e1b336234e81ac9cf9bc005969a556bbd0d2a29
Nazentcev Andrey <nazntrade@gmail.com>
2021-09-16 10:42:32 +0300

    began hw12

--- ActivityLifecycle/app/src/main/java/com/skillbox/homework12_12_Activity/MainActivity.kt
+++ ActivityLifecycle/app/src/main/java/com/skillbox/homework12_12_Activity/MainActivity.kt
@@ -3,13 +3,22 @@ package com.skillbox.homework12_12_Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.*
+import androidx.viewbinding.BuildConfig
 import com.skillbox.homework12_12_Activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        DebugLogger.v(tag, "onCreate ${hashCode()}")
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -55,4 +64,56 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.you_are_logged_in, Toast.LENGTH_LONG).show()
         }, 2000)
     }
+
+    override fun onStart() {
+        super.onStart()
+        DebugLogger.d(tag, "onStart ${hashCode()}")
+    }
+    override fun onResume() {
+        super.onResume()
+        DebugLogger.i(tag, "onResume ${hashCode()}")
+    }
+    override fun onPause() {
+        super.onPause()
+        DebugLogger.w(tag, "onPause ${hashCode()}")
+    }
+    override fun onStop() {
+        super.onStop()
+        DebugLogger.e(tag, "onStop ${hashCode()}")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        DebugLogger.v(tag, "onDestroy ${hashCode()}")
+    }
 }
+
+
+object DebugLogger {
+    fun v(tag: String, message: String) {
+        if (BuildConfig.DEBUG) {
+            Log.v(tag, message)
+        }
+    }
+    fun d(tag: String, message: String) {
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, message)
+        }
+    }
+    fun i(tag: String, message: String) {
+        if (BuildConfig.DEBUG) {
+            Log.i(tag, message)
+        }
+    }
+    fun w(tag: String, message: String) {
+        if (BuildConfig.DEBUG) {
+            Log.w(tag, message)
+        }
+    }
+    fun e(tag: String, message: String) {
+        if (BuildConfig.DEBUG) {
+            Log.e(tag, message)
+        }
+    }
+}
+
+
