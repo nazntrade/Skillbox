663afdb60c55935cfc3c01d0cad536229833003b
Andrew Nazentsev <nazntrade@gmail.com>
2021-09-29 15:36:06 +0300

    lesson 13.6 29/09

--- Android_Projects_All_Naz/AndroidStudio/Lessons13_NewScreens/app/src/main/java/com/example/lessons13_newscreens/MainActivity.kt
+++ Android_Projects_All_Naz/AndroidStudio/Lessons13_NewScreens/app/src/main/java/com/example/lessons13_newscreens/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.lessons13_newscreens
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +13,12 @@ import android.provider.MediaStore
 import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.lessons13_newscreens.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -23,6 +30,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Log.d("LifecycleTest", "MainActivity|onCreate|${hashCode()}")
+
+// в уроке этот метод. он больше не существует
+//      prepareCall(ActivityResultContracts.TakePicture()) {
+//                  bitmap ?: toast("Photo capture was cancelled")
+//            binding.resultPhotoImageView.setImageBitmap(bitmap)}
+//        вместо него новый (с добавл.в gradle)
+
+        val launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result != null) toast("Result : $result")
+                else toast("No Result")
+            }
+
 
         binding.startExplicitButton.setOnClickListener {
             val messageText = binding.messageEditText.text.toString()
@@ -59,13 +79,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun dispatchTakePictureIntent() {
 
-        //на нек.устр.необх.обяз разреш для исп камеры
-
-
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        //на нек.устр.необх.обяз разреш для исп камеры/проверка разреш
+        val isCameraPermissionGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        //если разреш нет - запрос разрешения
+        if (!isCameraPermissionGranted) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                14 //просто интовая константа,кот мы не используем
+            )
+        } else {//если разреш есть - откр камеру
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 //        check camera app
-        cameraIntent.resolveActivity(packageManager)?.also {
-            startActivityForResult(cameraIntent, PHOTO_REQUEST_CODE)
+            cameraIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(cameraIntent, PHOTO_REQUEST_CODE)
+            }
         }
     }
 
