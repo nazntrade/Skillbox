45b26a18e51b837f01b44af69bb399921bc3f556
Nazentsev_Andrey_Laptop_2 <nazntrade@gmail.com>
2021-12-27 11:06:03 +0300

    delete_if  -  hw_ViewModelAndNavigation: isn't Working

--- ViewModelAndNavigation/app/src/main/java/com/example/hw_ViewModelAndNavigation/petShop/PetShopListViewModel.kt
+++ ViewModelAndNavigation/app/src/main/java/com/example/hw_ViewModelAndNavigation/petShop/PetShopListViewModel.kt
@@ -7,18 +7,13 @@ class PetShopListViewModel : ViewModel() {
 
     private val repository = PetShopRepository()
     val animalsLiveData = MutableLiveData(repository.animals)
-
     val newAnimals: Animal.Cat = repository.addKitty()
+
     fun addAndUpdateListFun(): List<Animal> {
         val updateList = listOf(newAnimals) + animalsLiveData.value.orEmpty()
         animalsLiveData.postValue(updateList)
         return updateList
     }
-
-//    fun addKitty(): Animal.Cat {
-//
-//        return newAnimals
-//    }
 
     fun deleteAnimals(position: Int) {
         animalsLiveData.postValue(
