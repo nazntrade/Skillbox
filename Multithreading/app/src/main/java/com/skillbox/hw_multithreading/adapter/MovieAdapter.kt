c179f48b9e0411a63435a7b6d1fbbf53dd104edb
Nazentsev Andrew <nazntrade@gmail.com>
2022-01-24 10:38:08 +0300

    hw_multithreading 24/01/22

---
+++ Multithreading/app/src/main/java/com/skillbox/hw_multithreading/adapter/MovieAdapter.kt
@@ -0,0 +1,26 @@
+package com.skillbox.hw_multithreading.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.skillbox.hw_multithreading.databinding.MovieItemBinding
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(
+        binding: MovieItemBinding
+    ) : RecyclerView.ViewHolder(binding.root){
+
+    }
+}
--- Multithreading/app/src/main/java/com/skillbox/hw_multithreading/threading/ThreadingFragment.kt
+++ Multithreading/app/src/main/java/com/skillbox/hw_multithreading/threading/ThreadingFragment.kt
@@ -1,8 +1,28 @@
 package com.skillbox.hw_multithreading.threading
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.skillbox.hw_multithreading.R
+import com.skillbox.hw_multithreading.adapter.MovieAdapter
+import com.skillbox.hw_multithreading.databinding.FragmentThreadingBinding
 
 class ThreadingFragment : Fragment(R.layout.fragment_threading) {
 
+    lateinit var binding: FragmentThreadingBinding
+    var movieAdapter: MovieAdapter? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentThreadingBinding.bind(view)
+        initList()
+
+    }
+
+    private fun initList() {
+        movieAdapter = MovieAdapter()
+        with(binding.movieList) {
+            adapter = movieAdapter
+        }
+    }
 }
