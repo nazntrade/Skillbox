f1ba876b95bcea5ab22c49c350d9604491e19db2
Andrew Becker <nazntrade@gmail.com>
2022-04-28 14:37:04 +0300

    HW ContentProvider. Layout and Adapters is done

--- ContentProvider/app/src/main/java/com/example/hw_contentprovider/contacts/detailInfo/DetailContactInfoFragment.kt
+++ ContentProvider/app/src/main/java/com/example/hw_contentprovider/contacts/detailInfo/DetailContactInfoFragment.kt
@@ -1,8 +1,26 @@
 package com.example.hw_contentprovider.contacts.detailInfo
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.hw_contentprovider.R
+import com.example.hw_contentprovider.databinding.FragmentDetailInfoBinding
+import com.example.hw_contentprovider.databinding.FragmentItemContactBinding
 
 class DetailContactInfoFragment : Fragment(R.layout.fragment_detail_info) {
+
+    private val args: DetailContactInfoFragmentArgs by navArgs()
+    private lateinit var binding: FragmentDetailInfoBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailInfoBinding.bind(requireView())
+
+        binding.detailFirstNameTextView.text = args.currentContact.firstName
+        binding.detailSecondNameTextView.text = args.currentContact.secondName
+        binding.detailPhonesTextView.text = args.currentContact.phones.joinToString("\n")
+        binding.detailEmailTextView.text = args.currentContact.email
+    }
 
 }
