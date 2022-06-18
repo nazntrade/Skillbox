6c284bbc23158f632edd987c1bfc0d25e0ed9e61
Andrew Becker <nazntrade@gmail.com>
2022-06-18 09:03:23 +0300

    new: hwRoomDao. new files layout

--- RoomDao/app/src/main/java/com/example/hw_roomdao/presentation/chat_list/ChatListFragment.kt
+++ RoomDao/app/src/main/java/com/example/hw_roomdao/presentation/chat_list/ChatListFragment.kt
@@ -1,4 +1,25 @@
 package com.example.hw_roomdao.presentation.chat_list
 
-class chatListFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.hw_roomdao.R
+import com.example.hw_roomdao.databinding.FragmentChatListBinding
+
+class ChatListFragment: Fragment(R.layout.fragment_chat_list) {
+
+    private lateinit var binding: FragmentChatListBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentChatListBinding.bind(view)
+
+        initToolBar()
+
+    }
+
+    private fun initToolBar() {
+        binding.appBar.toolBar.title = getString(R.string.toolbar_chat_list_item)
+    }
+
 }
