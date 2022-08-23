77d4a116f01e48687d9eae4d010153e81ed6a9c0
Andrew Becker <nazntrade@gmail.com>
2022-08-23 10:46:20 +0300

    new: add permissions. HOW it works?

--- ScopedStorage/app/src/main/java/com/skillbox/hw_scopedstorage/presentation/videoList/VideoListFragment.kt
+++ ScopedStorage/app/src/main/java/com/skillbox/hw_scopedstorage/presentation/videoList/VideoListFragment.kt
@@ -1,27 +1,85 @@
 package com.skillbox.hw_scopedstorage.presentation.videoList
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.skillbox.hw_scopedstorage.R
 import com.skillbox.hw_scopedstorage.databinding.FragmentVideoListBinding
 import com.skillbox.hw_scopedstorage.utils.ViewBindingFragment
+import com.skillbox.hw_scopedstorage.utils.haveQ
 
-//since this time binding only this
+//since this time binding only this and +file: ViewBindingFragment
 class VideoListFragment :
     ViewBindingFragment<FragmentVideoListBinding>(FragmentVideoListBinding::inflate) {
+
+    private val viewModel: VideoListViewModel by viewModels()
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initPermissionResultListener()
+//        initRecoverableActionListener()
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initToolBar()
 
+        if (hasPermission().not()) {
+            requestPermissions()
+        }
+
     }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.updatePermissionState(hasPermission())
+    }
+
+    private fun initPermissionResultListener() {
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissionToGrantedMap: Map<String, Boolean> ->
+            if (permissionToGrantedMap.values.all { it }) {
+                viewModel.permissionsGranted()
+            } else {
+                viewModel.permissionsDenied()
+            }
+        }
+    }
+
+    private fun requestPermissions() {
+        requestPermissionLauncher.launch(*PERMISSIONS.toTypedArray())
+    }
+
+    private fun hasPermission(): Boolean {
+        return PERMISSIONS.all {
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
 
     //including statusBar
     private fun initToolBar() {
         binding.appBar.toolBar.title = getString(R.string.toolBarTitle)
     }
 
+    companion object {
+        private val PERMISSIONS = listOfNotNull(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                .takeIf { haveQ().not() }
+        )
+    }
 }
--- ScopedStorage/app/src/main/java/com/skillbox/hw_scopedstorage/presentation/videoList/VideoListViewModel.kt
+++ ScopedStorage/app/src/main/java/com/skillbox/hw_scopedstorage/presentation/videoList/VideoListViewModel.kt
@@ -1,4 +1,55 @@
 package com.skillbox.hw_scopedstorage.presentation.videoList
 
-class VideoListViewModel {
+import android.app.Application
+import android.app.RemoteAction
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.skillbox.hw_scopedstorage.data.Video
+import com.skillbox.hw_scopedstorage.utils.SingleLiveEvent
+
+class VideoListViewModel(
+    app: Application
+) : AndroidViewModel(app) {
+
+    private val permissionsGrantedMutableLiveData = MutableLiveData(true)
+    val permissionsGrantedLiveData: LiveData<Boolean>
+        get() = permissionsGrantedMutableLiveData
+
+    private val toastSingleLiveEvent = SingleLiveEvent<Int>()
+    val toastLiveData: LiveData<Int>
+        get() = toastSingleLiveEvent
+
+    private val imagesMutableLiveData = MutableLiveData<List<Video>>()
+    val imagesLiveData: LiveData<List<Video>>
+        get() = imagesMutableLiveData
+
+    private val recoverableActionMutableLiveData = MutableLiveData<RemoteAction>()
+    val recoverableActionLiveData: LiveData<RemoteAction>
+        get() = recoverableActionMutableLiveData
+
+    private var isObservingStarted: Boolean = false
+    private var pendingDeleteId: Long? = null
+
+    fun updatePermissionState(isGranted: Boolean) {
+        if (isGranted) {
+            permissionsGranted()
+        } else {
+            permissionsDenied()
+        }
+    }
+
+    fun permissionsGranted() {
+//        loadImages()
+        if (isObservingStarted.not()) {
+//            videoRepository.observeImages { loadImages() }
+            isObservingStarted = true
+        }
+        permissionsGrantedMutableLiveData.postValue(true)
+    }
+
+    fun permissionsDenied() {
+        permissionsGrantedMutableLiveData.postValue(false)
+    }
+
 }
