5959ee27b03b4a3e0540860d47c25eff0b86162d
Andrew Becker <nazntrade@gmail.com>
2022-11-21 08:43:16 +0300

    fix: menaged to cast one list from Jsom to another class in order to display

--- 2022/m19_location/app/src/main/java/com/skillbox/m19_location/data/AttractionsRepository.kt
+++ 2022/m19_location/app/src/main/java/com/skillbox/m19_location/data/AttractionsRepository.kt
@@ -1,12 +1,11 @@
 package com.skillbox.m19_location.data
 
-import com.skillbox.m19_location.entity.Attractions
 import javax.inject.Inject
 
 class AttractionsRepository @Inject constructor(
     private val attractionsDataSource: AttractionsDataSource
 ) {
     suspend fun getAttractions(): List<AttractionsDto> {
-        return attractionsDataSource.loadActivity()
+        return attractionsDataSource.loadAttractions()
     }
 }
