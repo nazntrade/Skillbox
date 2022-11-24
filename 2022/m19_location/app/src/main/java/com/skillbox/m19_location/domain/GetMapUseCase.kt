84573d5330fa96a3f65caf2f6ed580ab2c5b3cb5
Andrew Becker <nazntrade@gmail.com>
2022-11-24 15:46:47 +0300

    fix: map, myLocations, attractions is displayed correctly

--- 2022/m19_location/app/src/main/java/com/skillbox/m19_location/domain/GetMapUseCase.kt
+++ 2022/m19_location/app/src/main/java/com/skillbox/m19_location/domain/GetMapUseCase.kt
@@ -1,13 +1,15 @@
 package com.skillbox.m19_location.domain
 
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 import com.skillbox.m19_location.data.CurrentCoordinatesRepository
+import com.skillbox.m19_location.entity.Attractions
 import javax.inject.Inject
 
 class GetMapUseCase @Inject constructor(
     private val mapRepository: CurrentCoordinatesRepository
 ) {
-    suspend fun execute(myLat: Double, myLon: Double): OnMapReadyCallback {
-        return mapRepository.getMap(myLat, myLon)
+    suspend fun execute(myCoordinates: LatLng, attractions: List<Attractions>): OnMapReadyCallback {
+        return mapRepository.getMap(myCoordinates, attractions)
     }
 }
