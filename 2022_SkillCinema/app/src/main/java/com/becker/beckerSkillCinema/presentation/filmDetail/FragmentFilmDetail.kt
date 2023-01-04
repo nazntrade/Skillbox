2f91d0e3d301ad23cc2b4b7f595af7df65a19848
Andrew Becker <nazntrade@gmail.com>
2023-01-04 07:42:11 +0300

    fix: improved layout detail

---
+++ 2022_SkillCinema/app/src/main/java/com/becker/beckerSkillCinema/presentation/filmDetail/FragmentFilmDetail.kt
@@ -0,0 +1,10 @@
+package com.becker.beckerSkillCinema.presentation.filmDetail
+
+import com.becker.beckerSkillCinema.databinding.FragmentFilmDetailBinding
+import com.becker.beckerSkillCinema.presentation.ViewBindingFragment
+
+class FragmentFilmDetail :
+    ViewBindingFragment<FragmentFilmDetailBinding>(FragmentFilmDetailBinding::inflate) {
+
+
+}
